d11f1c4b21772cbdf8a2f18d12a5d174e0562a5f
LeDrumond <leandro.drumond@gmail.com>
2021-01-21 15:15:09 -0300

    samledevice

--- FrontEnd/src/app/home/home.component.ts
+++ FrontEnd/src/app/home/home.component.ts
@@ -1,15 +1,114 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+
+import {
+  PoDialogService,
+  PoModalComponent,
+  PoTableAction,
+  PoTableColumn,
+  PoTableComponent,
+  PoNotificationService
+} from '@po-ui/ng-components';
+
+import { SampleDevices } from './sampledevices';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent  implements OnInit {
+  actions: Array<PoTableAction> = [
+    {
+      action: this.discount.bind(this),
+      icon: 'po-icon-finance',
+      label: 'Apply Discount',
+      disabled: this.validateDiscount.bind(this)
+    },
+    { action: this.details.bind(this), icon: 'po-icon-info', label: 'Details' }
+  ];
+  columns: Array<PoTableColumn> = this.sampleDevices.getColumns();
+  detail: any;
+  items: Array<any> = this.sampleDevices.getItems();
+  total: number = 0;
+  totalExpanded = 0;
 
-  constructor() { }
+  @ViewChild(PoModalComponent, { static: true }) poModal: PoModalComponent;
+  @ViewChild(PoTableComponent, { static: true }) poTable: PoTableComponent;
+
+  constructor(
+    private sampleDevices: SampleDevices,
+    private poNotification: PoNotificationService,
+    private poDialog: PoDialogService
+  ) {}
 
   ngOnInit(): void {
   }
 
+  integrar() {
+
+  }
+
+  config() {
+
+  }
+
+  collapseAll() {
+    this.items.forEach((item, index) => {
+      if (item.detail) {
+        this.onCollapseDetail();
+        this.poTable.collapse(index);
+      }
+    });
+  }
+
+  decreaseTotal(row: any) {
+    if (row.value) {
+      this.total -= row.value;
+    }
+  }
+
+  details(item) {
+    this.detail = item;
+    this.poModal.open();
+  }
+
+  discount(item) {
+    if (!item.disableDiscount) {
+      item.value = item.value - item.value * 0.2;
+      item.disableDiscount = true;
+    }
+  }
+
+  expandAll() {
+    this.totalExpanded = 0;
+    this.items.forEach((item, index) => {
+      if (item.detail) {
+        this.onExpandDetail();
+        this.poTable.expand(index);
+      }
+    });
+  }
+
+  onCollapseDetail() {
+    this.totalExpanded -= 1;
+    this.totalExpanded = this.totalExpanded < 0 ? 0 : this.totalExpanded;
+  }
+
+  onExpandDetail() {
+    this.totalExpanded += 1;
+  }
+
+  sumTotal(row: any) {
+    if (row.value) {
+      this.total += row.value;
+    }
+  }
+
+  private getDescription(item: any) {
+    return `Airfare to ${item.destination} - ${item.initials}`;
+  }
+
+  private validateDiscount(item) {
+    return item.disableDiscount;
+  }
 }
